0cd1d0e78fc4eb0a0239369484af5a1cfcb69198
Stefaan Samyn <stefaan.samyn@hogent.be>
2018-09-28 12:21:30 +0200

    Creeer klasse BankAcount

---
+++ Banking2A2/Models/Domain/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Banking2A2.Models.Domain
+{
+    public class BankAccount
+    {
+        // private string _accountNumber;
+        // public const decimal WithdrawCost = 0.10M; 
+        // public static readonly decimal _withdrawCost = 0.10M;
+
+        #region Fields
+        private decimal _balance;
+
+        #endregion
+
+        #region Properties
+        public string AccountNumber { get; private set; }
+        public decimal Balance {
+            get {
+                return _balance;
+            }
+            private set {
+                if (value < 0)
+                    throw new ArgumentException("Balance cannot be negative");
+                _balance = value;
+            }
+        } 
+        #endregion
+
+        #region Constructors
+        public BankAccount(string accountNumber) : this(accountNumber, 0)
+        {
+        }
+
+        public BankAccount(string accountNumber, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount/*, int nrOfTimes = 1*/)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Amount cannot be negative");
+            Balance += amount/* * nrOfTimes*/;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Amount cannot be negative");
+            Balance -= amount;
+        } 
+        #endregion
+    }
+}
--- Banking2A2/Program.cs
+++ Banking2A2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking2A2.Models.Domain;
+using System;
 
 namespace Banking2A2
 {
@@ -6,7 +7,21 @@ namespace Banking2A2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myBA = new BankAccount("123-12312312-99");
+            Console.WriteLine($"Account {myBA.AccountNumber} created...");
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+ 
+            BankAccount myOtherBA = new BankAccount("123-12312312-99", 50);
+            Console.WriteLine($"Other account {myOtherBA.AccountNumber} created...");
+            Console.WriteLine($"Balance is currently {myOtherBA.Balance} Euro");
+
+            myBA.Deposit(1000);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Deposit(200);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Withdraw(100);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            Console.ReadKey();
         }
     }
 }
