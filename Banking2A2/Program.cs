96161e8b25171e944a45fb37ea6d02a6a576de28
Stefaan Samyn <stefaan.samyn@hogent.be>
2018-09-28 10:49:20 +0200

    Add project files.

---
+++ Banking2A2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banking2A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
